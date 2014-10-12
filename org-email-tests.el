e7630f583601dd2bfab3d857f7d2f3af646fed55
Nic Ferrier <nferrier@ferrier.me.uk>
2014-10-12 02:54:42 +0100

    remove old test. add new test.

--- org-email-tests.el
+++ org-email-tests.el
@@ -3,33 +3,23 @@
 (require 'ert)
 (require 'org-email)
 
-(ert-deftest org-email-test-structure ()
-  "Tests that the expected structure can be navigated properly."
-  (with-temp-buffer
-   (insert "* bill the buck
-** email
-*** billbuck@example1.com
-** partner
-*** Gillie The Girl
-* lesley lady
-** colleague @ woomedia
-*** CSS programmer
-** partner
-*** Jimmy Screws
-** email
-*** ll@example10.org")
-   (org-mode)
-   ;; Have we got the basics right?
-   (save-excursion
-     (goto-char (point-min))
-     (should (equal "* bill the buck" 
-                    (buffer-substring-no-properties (point-min)(line-end-position)))))
-   ;; Now pull the emails and check two
-   (let* ((emails (org-email--buffer-emails (current-buffer)))
-          (bill (assoc "bill the buck" emails))
-          (lesley (assoc "lesley lady" emails)))
-     (should (equal "billbuck@example1.com" (cdr bill)))
-     (should (equal "ll@example10.org" (cdr lesley))))))
+(defconst org-email-tests-dir
+  (file-name-directory
+   (or (buffer-file-name)
+       load-file-name
+       default-directory)))
+
+(ert-deftest org-email-lists ()
+  "Test the parsing of org files with emails."
+  (should
+   (equal
+    (org-email-list
+     (expand-file-name "test.org" org-email-tests-dir))
+    '(("nic ferrier" . "nic@ferrier.me.uk")
+      ("J Love" . "j.luv@somemail.com")
+      ("Bob Towney" . "B.towney@digital.place.email.uk")
+      ("Some Guy" . "guy@some.org")))))
+
 
 
 ;;; org-email-tests.el ends here
